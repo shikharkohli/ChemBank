82f23ef06eb2560830914063ee9169acc64b5041
Shikhar Kohli <shikharkohli@gmail.com>
2018-07-29 16:19:19 +0530

    Some changes to build Android and allow live reloading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { ExamHelpPage } from '../pages/exam-help/exam-help';
+import { ReadyToStudyPage } from '../pages/ready-to-study/ready-to-study';
+import { ExamTimePage } from '../pages/exam-time/exam-time';
+import { PostResultStressPage } from '../pages/post-result-stress/post-result-stress';
+import { YouTubeLinksPage } from '../pages/you-tube-links/you-tube-links';
+import { SymptomsOfExamStressPage } from '../pages/symptoms-of-exam-stress/symptoms-of-exam-stress';
+import { TimeManagementTipsPage } from '../pages/time-management-tips/time-management-tips';
+import { HeaderMenuComponent } from '../components/header-menu/header-menu';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    ExamHelpPage,
+    ReadyToStudyPage,
+    ExamTimePage,
+    PostResultStressPage,
+    YouTubeLinksPage,
+    SymptomsOfExamStressPage,
+    TimeManagementTipsPage,
+    HeaderMenuComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +34,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    ExamHelpPage,
+    ReadyToStudyPage,
+    ExamTimePage,
+    PostResultStressPage,
+    YouTubeLinksPage,
+    SymptomsOfExamStressPage,
+    TimeManagementTipsPage
   ],
   providers: [
     StatusBar,
